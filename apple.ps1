a53bceb0068d7299effb01f9fbf832422f2cce67
agnaszip <119476365+agnaszip@users.noreply.github.com>
2024-07-17 19:56:29 +0900

    Update apple.ps1

--- apple.ps1
+++ apple.ps1
@@ -1,3 +1,4 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 mkdir "C:\ProgramData\AnyDesk"
 $clnt=new-object System.Net.WebClient
 $url="http://download.anydesk.com/AnyDesk.exe"
